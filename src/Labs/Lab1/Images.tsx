543f2988da37b956ccc598c004287b66e7ea4668
AbdelrahmanZeidan5 <zeidan.ab@northeastern.edu>
2024-07-05 23:16:45 +0300

    add alt attribute to images

--- src/Labs/Lab1/Images.tsx
+++ src/Labs/Lab1/Images.tsx
@@ -4,10 +4,10 @@ export default function Images() {
             <h4>Image tag</h4>
             Loading an image from the internet:
             <br />
-            <img id="wd-starship" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+            <img id="wd-starship" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="starship" />
             <br />
             Loading a local image: <br />
-            <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+            <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="teslabot"/>
         </div>
     )
 }
